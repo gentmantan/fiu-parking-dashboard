b624ba59702f4c9bb1ea07af2eae421127270f2f
FIU Parking <fiu88080@fiu.edu>
2025-11-21 09:53:33 -0500

    Add action url parameter

--- source/actions.sql
+++ source/actions.sql
@@ -1,6 +1,13 @@
 DELETE FROM ManualCounts 
-WHERE count_id = $delete_id::int
+WHERE $action = 'delete' AND count_id = $delete_id::int
 RETURNING 'redirect' AS component, 'index.sql' AS link;
 
-INSERT INTO ManualCounts(garage_id, count_real, count_system, timestamp) VALUES (:garage_id::int, :count_real::int, :count_system::int, CURRENT_TIMESTAMP)
+INSERT INTO ManualCounts(garage_id, count_real, count_system, timestamp)
+SELECT 
+    :garage_id::int, 
+    :count_real::int,
+    :count_system::int,
+    CURRENT_TIMESTAMP
+WHERE 
+    $action = 'insert'
 RETURNING 'redirect' AS component, 'index.sql' AS link;
--- source/index.sql
+++ source/index.sql
@@ -1,14 +1,16 @@
 select 
   'shell' as component,
-  'FIU Parking & Transportation' as title,
+  'FIU Parking' as title,
   'paw' as icon,
   '/' as link,
-  JSON('{"title":"Manual Count Form","active":true}') as menu_item;
+  JSON('[{"link":"/","title":"Manual Count Form","active":true},{"link":"/reports.sql","title":"Reports","active":false}]') as menu_item,
+  TRUE as sidebar,
+  'https://www.fiu.edu/_assets/images/favicon.ico' as favicon;
 
 select 'form' as component,
   'Occupancy Form' as title,
   'Enter Count' as validate,
-  'actions.sql' as action;
+  'actions.sql?action=insert' as action;
 
 select 'garage_id' as name, 
   'select' as type, 
@@ -29,12 +31,11 @@ select 'count_system' as name,
 select 'table' as component,
   true as sort,
   true as search,
-  CURRENT_DATE as initial_search_value,
-  '/actions.sql?delete_id={id}' as delete_url;
+  to_char(CURRENT_TIMESTAMP, 'MM-DD-YYYY') as initial_search_value,
+  '/actions.sql?action=delete&delete_id={id}' as delete_url;
 select t1.count_id as _sqlpage_id,
   t2.garage_name as Garage,
   t1.count_real as Real,
   t1.count_system as System,
-  -- to_char(timezone('America/New_York', t1.timestamp), 'MM-DD-YYYY HH:MM:SS') as Time
-  timezone('America/New_York', t1.timestamp) as Time
+  to_char((t1.timestamp AT TIME ZONE 'EDT'), 'MM-DD-YYYY HH:MM:SS') as Time
 from ManualCounts t1 JOIN Garages t2 ON t1.garage_id = t2.garage_id;
