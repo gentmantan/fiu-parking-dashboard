36ec28094d4a84885ab4144019eacdf59f4b89a2
FIU Parking <fiu88080@fiu.edu>
2025-11-21 14:53:07 -0500

    Add report exporting function

---
+++ source/report.sql
@@ -0,0 +1,38 @@
+select 
+  'shell' as component,
+  'FIU Parking' as title,
+  'paw' as icon,
+  '/' as link,
+  JSON('[{"link":"/","title":"Manual Count Form","active":false},{"link":"/reports.sql","title":"Reports","active":false}]') as menu_item,
+  TRUE as sidebar,
+  'https://www.fiu.edu/_assets/images/favicon.ico' as favicon;
+
+select 'button' as component;
+select 'Return' as title,
+  'arrow-left' as icon,
+  '/reports.sql' as link;
+select 'csv' as component,
+  'Download report' as title,
+  'file-download' as icon,
+  'green' as color,
+  TRUE as bom,
+  'occupancy_report' || to_char(to_timestamp(:from_date,'YYYY-MM-DD'), 'YYYYMMDD' || '-' || to_char(to_timestamp(:to_date, 'YYYY-MM-DD'), 'YYYYMMDD')) as filename;
+SELECT t2.garage_name as Garage,
+  t1.count_real as Real,
+  t1.count_system as System,
+  to_char((t1.timestamp AT TIME ZONE 'EDT'), 'MM-DD-YYYY HH:MM:SS') as Time
+FROM ManualCounts t1 JOIN Garages t2 ON t1.garage_id = t2.garage_id
+WHERE timestamp >= to_timestamp(:from_date, 'YYYY-MM-DD') AND 
+timestamp <= to_timestamp(:to_date, 'YYYY-MM-DD');
+
+select 'table' as component,
+  TRUE as small,
+  TRUE as sort;
+SELECT t1.count_id AS _sqlpage_id,
+  t2.garage_name as Garage,
+  t1.count_real as Real,
+  t1.count_system as System,
+  to_char((t1.timestamp AT TIME ZONE 'EDT'), 'MM-DD-YYYY HH:MM:SS') as Time
+FROM ManualCounts t1 JOIN Garages t2 ON t1.garage_id = t2.garage_id
+WHERE timestamp >= to_timestamp(:from_date, 'YYYY-MM-DD') AND 
+timestamp <= to_timestamp(:to_date, 'YYYY-MM-DD');
---
+++ source/reports.sql
@@ -0,0 +1,25 @@
+select 
+  'shell' as component,
+  'FIU Parking' as title,
+  'paw' as icon,
+  '/' as link,
+  JSON('[{"link":"/","title":"Manual Count Form","active":false},{"link":"/reports.sql","title":"Reports","active":true}]') as menu_item,
+  TRUE as sidebar,
+  'https://www.fiu.edu/_assets/images/favicon.ico' as favicon;
+
+select
+  'form' as component,
+  'Generate Report' as title,
+  'Create report' as validate,
+  '/report.sql' as action;
+select
+  'from_date' as name,
+  'From date' as label,
+  'date' as type,
+  (select to_char((CURRENT_TIMESTAMP AT TIME ZONE 'EDT'), 'YYYY-MM-DD')) as value;
+  /* (select to_char(date_trunc('week', (CURRENT_TIMESTAMP AT TIME ZONE 'EDT'))), 'YYYY-MM-DD') as value; */
+select
+  'to_date' as name,
+  'To date' as label,
+  'date' as type,
+  (select to_char((CURRENT_TIMESTAMP AT TIME ZONE 'EDT') + interval '1 day', 'YYYY-MM-DD')) as value;
